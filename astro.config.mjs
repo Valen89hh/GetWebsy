c422374e832af1f95562577b4c847a7dda0cbf2b
valentino <126835108+Valen89hh@users.noreply.github.com>
2025-07-16 13:01:09 -0500

    fix output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
+import netlify from '@astrojs/netlify';
 import react from "@astrojs/react";
 
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // 👈 obligatorio para usar funciones tipo API
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [react()]
 });
